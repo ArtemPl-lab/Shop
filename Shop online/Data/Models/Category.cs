5ee98decb83feb3c118363ea7adb560f03284c23
artem <platonovartem2005@outlook.com>
2020-04-25 13:29:27 +0300

    Добавьте файлы проекта.

---
+++ Shop online/Data/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop_online.Data.Models
+{
+    public class Category
+    {
+        public int id { set; get; }
+        public string categoryName { set; get; }
+        public int desc { set; get; }
+       // public int id { set; get; }
+
+    }
+}
