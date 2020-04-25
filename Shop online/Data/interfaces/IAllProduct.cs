e1a61adf58aeb233a578a2c141776303461b9a5c
artem <platonovartem2005@outlook.com>
2020-04-25 14:36:42 +0300

    Refactoring...

--- Shop online/Data/interfaces/IAllProduct.cs
+++ Shop online/Data/interfaces/IAllProduct.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shop_online.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,6 @@ namespace Shop_online.Data.interfaces
 {
     interface IAllProduct
     {
+        IEnumerable<Category> AllCategories { get; }
     }
 }
--- Shop online/Data/interfaces/IProducCategory.cs
+++ Shop online/Data/interfaces/IProducCategory.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Shop_online.Data.Models;
 namespace Shop_online.Data.interfaces
 {
     interface IProducCategory
     {
+        IEnumerable<Instance> Products { get; set; }
+        IEnumerable<Instance> FavouriteProducts { get; set; }
+        Instance getObjectProduct(int id);
     }
 }
